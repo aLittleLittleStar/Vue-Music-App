cf3efc2fa0f07d43fb73265338f217f3ef3a45e4
aLittleLittleStar <2917615487@qq.com>
2018-08-23 10:48:55 +0800

    歌手页面

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,17 +1,97 @@
 <template>
-	<div>
-		歌手页面
+	<div class="singer">
+		<list-view :data="singers"></list-view>
 	</div>
 </template>
 
 <script>
+	import { getSingerList } from '@/api/singer'
+	import { ERRR_OK } from '@/api/config'
+	import Singer from "@/common/js/singer"
+	import ListView from  '@/base/listview/listview'
+
+	const HOT_NAME = '热门'
+	const HOT_SINGER_LEN = 10
+
 	export default {
-		
+		data () {
+			return {
+				singers: []
+			}
+		},
+		created () {
+			this._getSingerList()
+		},
+		methods: {
+			_getSingerList() {
+				getSingerList().then((res) => {
+					if (res.conde === ERRR_OK) {
+						this.singers = this._nomalizeSinger(res.data.list)
+						// console.log(this.singers);
+					}
+				})
+			},
+			_nomalizeSinger(list) {
+				let map = {
+					hot: {
+						title: HOT_NAME,
+						items: []
+					}
+				}
+				list.forEach( (item, index) => {
+					if ( index < HOT_SINGER_LEN) {
+						map.hot.items.push(new Singer({
+							id: item.Fsinger_mid,
+							name: item.Fsinger_name
+						}))
+					}
+
+					const key = item.Findex
+					if(!map[key]) {
+						map[key] = {
+							title: key,
+							items: []
+						}
+					}
+
+					map[key].items.push(new Singer ({
+						id: item.Fsinger_mid,
+						name: item.Fsinger_name
+					}))
+				})
+
+				// 为了得到有序列表，我们需要处理 map
+				let hot = []
+				let ret = []
+
+				for (let key in map) {
+					let val = map[key]
+					if ( val.title.match(/[a-zA-Z]/)) {
+						ret.push(val)
+					}else if (val.title === HOT_NAME) {
+						hot.push(val)
+					}
+				}
+
+				ret.sort( (a, b) => {
+					return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+				})
+				return hot.concat(ret)
+			}
+		},
+		components: {
+			ListView
+		}
 	}
 </script>
 
 <style lang="stylus" scoped>
 	@import '~common/stylus/variable'
 	@import '~common/stylus/mixin'
-		
+	
+	.singer
+		position: fixed
+		top: 88px
+		bottom: 0
+		width: 100%
 </style>
